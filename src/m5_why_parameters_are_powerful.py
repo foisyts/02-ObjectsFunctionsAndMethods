b46f1ba663567da4c0704917fd7351bfbfa5f612
foisyts <foisyts@rose-hulman.edu>
2018-11-28 19:08:22 -0500

    Finished

--- src/m5_why_parameters_are_powerful.py
+++ src/m5_why_parameters_are_powerful.py
@@ -215,7 +215,9 @@ def even_better_draw_circles(point, growth, number, color, thickness):
         turtle.draw_circle(growth * k)
 
     ###############################################################################
-# TODO: 5.
+
+
+# DONE: 5.
 #
 # Finally, comment-out the existing calls to  even_better_draw_circles  and
 # add code in   main  to draw various circles that form a BEAUTIFUL picture!
